1ea10f9a90102eeb5bcecd454e2b6754671a1a08
Raiyan Ahmed <ray38250968@gmail.com>
2025-01-09 10:09:16 +0530

    worked on <Profiler>

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useSyncExternalStore } from "react";
+import React, { Profiler, useReducer, useSyncExternalStore } from "react";
 import userOnlineStatus from "./userOnlineStatus";
 
 function reducer(STATE, ACTION) {
@@ -29,35 +29,46 @@ export default function Todo() {
         userOnlineStatus.subscribe,
         userOnlineStatus.getSnapShot
     );
+    function onRender(id, phase, actualDuration, baseDuration, startTime, commitTime) {
+        console.log("id: ", id);
+        console.log("phase: ", phase);
+        console.log("actualDuration: ", actualDuration);
+        console.log("baseDuration: ", baseDuration);
+        console.log("startTime: ", startTime);
+        console.log("commitTime: ", commitTime);
+    }
+
     return (
-        <div>
-            {isOnline ? <h1>You are online</h1> : <h1>You are Offline</h1>}
-            <h1>TODO</h1>
-            <form action={formAction}>
-                <input
-                    type="text"
-                    name="todoInput"
-                    className="input input-sm input-bordered w-full max-w-xs"
-                />
-                <input
-                    type="submit"
-                    value="Add"
-                    className="btn btn-sm btn-bordered  max-w-xs ml-2"
-                />
-            </form>
-            {false && <h1>Loading</h1>}
-            <ul className="p-4">
-                {state.map((todoItem, todoKey) => {
-                    return (
-                        <li
-                            className="hover:bg-base-200 px-3 py-2 rounded"
-                            key={todoKey + todoItem}
-                        >
-                            {todoItem}
-                        </li>
-                    );
-                })}
-            </ul>
-        </div>
+        <Profiler id="main" onRender={onRender}>
+            <div>
+                {isOnline ? <h1>You are online</h1> : <h1>You are Offline</h1>}
+                <h1>TODO</h1>
+                <form action={formAction}>
+                    <input
+                        type="text"
+                        name="todoInput"
+                        className="input input-sm input-bordered w-full max-w-xs"
+                    />
+                    <input
+                        type="submit"
+                        value="Add"
+                        className="btn btn-sm btn-bordered  max-w-xs ml-2"
+                    />
+                </form>
+                {false && <h1>Loading</h1>}
+                <ul className="p-4">
+                    {state.map((todoItem, todoKey) => {
+                        return (
+                            <li
+                                className="hover:bg-base-200 px-3 py-2 rounded"
+                                key={todoKey + todoItem}
+                            >
+                                {todoItem}
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </Profiler>
     );
 }
