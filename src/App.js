38a0b2b91591a253234a86aa6f34bb3a67f93675
Raiyan Ahmed <ray38250968@gmail.com>
2025-01-09 17:50:56 +0530

    worked on memo API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import React, { useTransition, useState, useEffect, act } from 'react'
-import Todo from './Todo';
-import Box from './Box';
+import React, { useState, useEffect, memo } from 'react'
 
 export default function App() {
     const [count, setCount] = useState(0);
@@ -20,6 +18,59 @@ export default function App() {
                     Click me
                 </button>
             </div>
+            <MyApp />
         </div>
     )
+}
+
+export function MyApp() {
+    const [name, setName] = useState('');
+    const [address, setAddress] = useState('');
+    return (
+        <>
+            <label>
+                Name{': '}
+                <input className='input input-bordered' value={name} onChange={e => setName(e.target.value)} />
+            </label>
+            <label>
+                Address{': '}
+                <input className='input input-bordered' value={address} onChange={e => setAddress(e.target.value)} />
+            </label>
+            <Greeting name={name} />
+        </>
+    );
+}
+
+const Greeting = memo(function Greeting({ name }) {
+    console.log('Greeting was rendered at', new Date().toLocaleTimeString());
+    const [greeting, setGreeting] = useState('Hello');
+    return (
+        <>
+            <h3>{greeting}{name && ', '}{name}!</h3>
+            <GreetingSelector value={greeting} onChange={setGreeting} />
+        </>
+    );
+});
+
+function GreetingSelector({ value, onChange }) {
+    return (
+        <>
+            <label>
+                <input
+                    type="radio"
+                    checked={value === 'Hello'}
+                    onChange={e => onChange('Hello')}
+                />
+                Regular greeting
+            </label>
+            <label>
+                <input
+                    type="radio"
+                    checked={value === 'Hello and welcome'}
+                    onChange={e => onChange('Hello and welcome')}
+                />
+                Enthusiastic greeting
+            </label>
+        </>
+    );
 }
