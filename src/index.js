a052905d6d2eba2ab0f6d3a9360a563b3c1b56c1
Raiyan Ahmed <ray38250968@gmail.com>
2025-01-09 16:42:57 +0530

    worked on react/lazy API and removed test codes

--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
-import React, { StrictMode, act } from "react";
+import React, { StrictMode, lazy } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "./useTheme";
-import App from "./App";
 import "./index.css";
+const App = lazy(() => import("./App"))
 
-// To describe : execution is in testing ACT-MODE
-global.IS_REACT_ACT_ENVIRONMENT = true;
 
-const container = document.getElementById("root");
-
-await act(() => {
-    createRoot(container).render(
-        <StrictMode>
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        </StrictMode>
-    )
-});
-
-const button = container.querySelector('button');
-
-if (document.title === 'You clicked 0 times') {
-    console.log("success")
-} else {
-    console.log("Failed")
-}
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
+    </StrictMode>
+)
