286148e5c0f7c935509bea681c90b53f08d28eef
Raiyan Ahmed <ray38250968@gmail.com>
2025-01-10 12:32:46 +0530

    worked on createPortal

--- src/App.js
+++ src/App.js
@@ -1,78 +1,17 @@
-import React, { useState, useEffect, memo, use } from 'react'
-import { ThemeContext } from './useTheme';
+import React from "react";
+import { createPortal } from "react-dom";
 export default function App() {
-    const [count, setCount] = useState(0);
-    const handleClick = () => {
-        setCount(prev => prev + 1);
-    }
-
-    useEffect(() => {
-        document.title = `You clicked ${count} times`;
-    }, [count]);
-    console.log(use(ThemeContext));
-
     return (
-        <div className='prose lg:prose-xl'>
+        <div className="prose lg:prose-xl">
+            <h1>Helllo there from Parents</h1>
             <div>
-                <h1 className='cursor-pointer select-none' onClick={use(ThemeContext).SwitchTheme}>Theme : {use(ThemeContext).Theme} </h1>
-                <h1 className=''>You clicked {count} times</h1>
-                <button className='btn' onClick={handleClick}>
-                    Click me
-                </button>
+                {createPortal(
+                    <h1>
+                        Hey there!, From another already exisiting <b>DOM Element.</b>
+                    </h1>,
+                    document.getElementById("secondRoot")
+                )}
             </div>
-            <MyApp />
         </div>
-    )
-}
-
-export function MyApp() {
-    const [name, setName] = useState('');
-    const [address, setAddress] = useState('');
-    return (
-        <>
-            <label>
-                Name{': '}
-                <input className='input input-bordered' value={name} onChange={e => setName(e.target.value)} />
-            </label>
-            <label>
-                Address{': '}
-                <input className='input input-bordered' value={address} onChange={e => setAddress(e.target.value)} />
-            </label>
-            <Greeting name={name} />
-        </>
-    );
-}
-
-const Greeting = memo(function Greeting({ name }) {
-    console.log('Greeting was rendered at', new Date().toLocaleTimeString());
-    const [greeting, setGreeting] = useState('Hello');
-    return (
-        <>
-            <h3>{greeting}{name && ', '}{name}!</h3>
-            <GreetingSelector value={greeting} onChange={setGreeting} />
-        </>
-    );
-});
-
-function GreetingSelector({ value, onChange }) {
-    return (
-        <>
-            <label>
-                <input
-                    type="radio"
-                    checked={value === 'Hello'}
-                    onChange={e => onChange('Hello')}
-                />
-                Regular greeting
-            </label>
-            <label>
-                <input
-                    type="radio"
-                    checked={value === 'Hello and welcome'}
-                    onChange={e => onChange('Hello and welcome')}
-                />
-                Enthusiastic greeting
-            </label>
-        </>
     );
 }
